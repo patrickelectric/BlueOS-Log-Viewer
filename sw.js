2eaf210a79459e97be74830f0e722b401a3512dd
patrickelectric <patrickelectric@gmail.com>
2024-04-30 05:43:24 +0000

    Deploying to gh-pages from @ patrickelectric/BlueOS-Log-Viewer@6e2fd24e57fab05e8bbc4e35d6fa972e75d6eb00 🚀

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './blueos_log_viewer.js',
+  './blueos_log_viewer_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
